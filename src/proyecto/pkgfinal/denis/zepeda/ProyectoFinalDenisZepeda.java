02d99c63e5212b6190a43f61fa6e1449bd0cafb5
Koxtt <itzjosue06@gmail.com>
2025-12-16 23:58:26 -0600

    fourth commit

--- src/proyecto/pkgfinal/denis/zepeda/ProyectoFinalDenisZepeda.java
+++ src/proyecto/pkgfinal/denis/zepeda/ProyectoFinalDenisZepeda.java
@@ -57,30 +57,30 @@ private char[][] tablero;
     }
     
         public void inicializarTablero() {
-            for (int i = 0; i > tamaño; i++) {
-                for (int j = 0; j < tamaño; j++) {
-                    if (i == 0||j==tamaño-1|j==0||j==tamaño-1) {
-                        tablero[i][j]='#'; 
-                    } else {
-                        tablero[i][j ='.'; 
-                    }
-                }
-            }
-        }
+               for (int i = 0; i < tamaño; i++) {
+                   for (int j = 0; j < tamaño; j++) {
+                    if (i == 0 || i == tamaño - 1 || j == 0 || j == tamaño - 1) {
+                           tablero[i][j] = '#'; 
+                       } else {
+                           tablero[i][j] = '.';
+                       }
+                   }
+               }
+           }
         
         public void dibujarTablero() {
-        System.out.println("\n\n\n\n\n\n\n\n\n"); 
+        System.out.println("\n");
         System.out.println("PUNTOS: " + puntos);
         for (int i = 0; i < tamaño; i++) {
             for (int j = 0; j < tamaño; j++) {
                 if (i == pacmanX && j == pacmanY) {
-                    System.out.print("C "); 
+                    System.out.print(":v "); //Pacman
                 } else {
                    
                     boolean hayFantasma = false;
                     for (int f = 0; f < cantidadFantasmas; f++) {
                         if (fantasmas[f][0] == i && fantasmas[f][1] == j) {
-                            System.out.print("G "); 
+                            System.out.print("G "); //Fantasmas
                             hayFantasma = true;
                             break;
                         }
@@ -91,6 +91,30 @@ private char[][] tablero;
             System.out.println();
         }
     }
+        
+        public void moverPacman(int direccion) {
+        int proximaX = pacmanX;
+        int proximaY = pacmanY;
+
+        if (direccion == 1) proximaX--;      
+        else 
+            if (direccion == 2) proximaX++; 
+        else 
+                if (direccion == 3) proximaY--;
+        else
+                    if (direccion == 4) proximaY++;
+
+        if (tablero[proximaX][proximaY] != '#') {
+            pacmanX = proximaX;
+            pacmanY = proximaY;
+            if (tablero[pacmanX][pacmanY] == '.') {
+                tablero[pacmanX][pacmanY] = ' '; 
+                puntos += 10;
+            }
+        }
+    }
+        
+
         
         public static void main(String[] args) {
             Scanner entry = new Scanner(System.in);
